97e4f4f98c68e0a6e38601240e988bc62b6e2d63
GermanGri <german.gribanov@gmail.com>
2024-01-29 22:48:47 +0300

    Finished 2

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-
+// changes for pull request
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
     @Mock
