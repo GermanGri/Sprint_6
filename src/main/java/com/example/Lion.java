e0d3fc8c095dde4921e5aa812fde0b582ec05087
GermanGri <german.gribanov@gmail.com>
2024-01-31 19:00:51 +0300

    Revert "Finished"
    This reverts commit a647dc0de4b4a9e1757420762d23242b219ea278.

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,12 +5,8 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    // добавил
-    private final Feline feline;
 
-
-    // изменил
-    public Lion(String sex, Feline feline) throws Exception {
+    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -18,11 +14,9 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
-        this.feline = feline;
     }
 
-    //закоментил
-    //Feline feline = new Feline();
+    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
