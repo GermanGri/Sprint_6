50dc9ef630726401c95a89878061f5e970ea5e81
GermanGri <german.gribanov@gmail.com>
2024-02-03 13:43:00 +0300

    fix param test

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -25,12 +25,16 @@ public class FelineTest {
         String result = animal.getFamily();
         assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи", result);
     }
-
     @Test
     public void testGetKittens(){
         Feline feline = new Feline();
         int result = feline.getKittens();
         assertEquals(1, result);
+    }
+    @Test(expected = Exception.class)
+    public void testUnknownAnimalKind() throws Exception {
+        Feline feline = new Feline();
+        feline.getFood("НеизвестныйВид");
     }
 
 }
